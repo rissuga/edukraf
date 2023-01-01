3a0a4b3df2679cd41f4e993b9b00eae294d695b3
Rahmat Ramadhan Putra <rahmatramadhan.ti.poliwangi@gmail.com>
2023-01-01 21:22:16 +0700

    fix minor bugs

--- resources/views/frontend/body/footer.blade.php
+++ resources/views/frontend/body/footer.blade.php
@@ -39,7 +39,7 @@
     <div class="container d-md-flex py-4">
         <div class="me-md-auto text-center text-md-start">
             <div class="copyright" style="color: white;">
-                &copy; Copyright 2022 BWI-JBR 2 Kelompok 1
+                &copy; Copyright 2023  <br> Made with 💖 <br> Project Studi Independen at SEAL (Social Economic Accelerator Labs)
             </div>
         </div>
         <div class="social-links text-center text-md-right pt-3 pt-md-0">
